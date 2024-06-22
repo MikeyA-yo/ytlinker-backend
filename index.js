82568204f509716ed4ae512c22c921eb6b5ad1ad
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-06-22 17:02:17 +0100

    update cors option

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app = express();
 const corsOption = {
     origin:"http://localhost:5173/"
 }
-app.use(cors())
+app.use(cors(
+    {
+        origin:"http://localhost:5173/"
+    }
+))
 
 function genTimeStamp(time){
     let hour;
