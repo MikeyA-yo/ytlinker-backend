7446029c8ccbe0094928dd0824979a85eedf8ae2
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-06-23 18:51:28 +0100

    please don't make me cry

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const app = express();
 //     origin:"http://localhost:5173"
 // }
 app.use(cors())
-
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 function genTimeStamp(time){
     let hour;
     let minutes;
